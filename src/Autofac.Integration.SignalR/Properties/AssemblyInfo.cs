bbdafb8e0d6c9976f4e528c8c95347bb9ab2bc64
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-01-29 00:05:15 +1000

    Added solution and build configuration for Autofac.SignalR integration.

--- src/Autofac.Integration.SignalR/Properties/AssemblyInfo.cs
+++ src/Autofac.Integration.SignalR/Properties/AssemblyInfo.cs
@@ -1,9 +1,21 @@
 ﻿using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Autofac.Integration.SignalR")]
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)] // SignalR is not marked CLS compliant.
 
-// SignalR is not marked CLS compliant.
-[assembly: CLSCompliant(false)]
+[assembly: AssemblyCompany("Autofac Project - http://autofac.org")]
+[assembly: AssemblyProduct("Autofac")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Copyright © 2014 Autofac Contributors")]
+[assembly: AssemblyDescription("Autofac SignalR Integration")]
